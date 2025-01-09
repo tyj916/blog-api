956b74b6013e2675a60b2cf5b7c153e10ff46805
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-09 16:54:46 +0800

    Move app file to src

---
+++ src/app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use('/posts', routes.post);
+app.use('/users', routes.user);
+app.use('/posts/:postId/comments', routes.comment);
+
+app.listen(process.env.PORT, () => console.log(`Listening to port ${process.env.PORT}...`));
