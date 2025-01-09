0e4d3bbe9da4f5037dbe35eafba82ac3d3b06b9e
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-09 16:53:14 +0800

    Create database queries file

---
+++ src/models/index.js
@@ -0,0 +1,15 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+module.exports = {
+
+}
