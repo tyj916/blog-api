f7cba8fc9e64102df43171649775aa786e20096b
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-09 17:47:14 +0800

    Add create post api

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -9,6 +9,21 @@ async function getAllPosts(req, res) {
   }
 }
 
+async function createPost(req, res) {
+  try {
+    const data = {
+      title: req.body.title,
+      content: req.body.content,
+      status: req.body.status,
+      authorId: req.user.id,
+    }
+    return await db.insertPost(data);
+  } catch(err) {
+    console.error(err);
+  }
+}
+
 module.exports = {
   getAllPosts,
+  createPost,
 }
--- src/routes/post.js
+++ src/routes/post.js
@@ -4,5 +4,6 @@ const controllers = require('../controllers');
 const router = Router();
 
 router.get('/', controllers.post.getAllPosts);
+router.post('/', controllers.post.createPost);
 
 module.exports = router;
