55a7e43ac191443e76d83ad0186f7c8ef4b0683d
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-15 21:01:12 +0800

    Add get routers to all access

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 const { Router } = require('express');
-const passport = require('passport');
 const auth = require('./auth');
 const post = require('./post');
 const user = require('./user');
@@ -8,7 +7,6 @@ const comment = require('./comment');
 const router = Router();
 
 router.use('/', auth);
-router.use(passport.authenticate('jwt', { session: false }));
 router.use('/posts', post);
 router.use('/users', user);
 router.use('/posts/:postId/comments', comment);
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
+const passport = require('passport');
 const controllers = require('../controllers');
 
 const router = Router();
 
 router.get('/', controllers.post.getAllPosts);
 router.get('/:postId', controllers.post.getPostByPostId);
+
+router.use(passport.authenticate('jwt', { session: false }));
+
 router.post('/', controllers.post.createPost);
 router.put('/:postId', controllers.post.updatePost);
 router.delete('/:postId', controllers.post.deletePost);
