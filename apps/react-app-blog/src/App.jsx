8abfac941c15d3bc554bbb2dd65fcc68a6a2814d
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-16 22:42:23 +0800

    Add header component

--- apps/react-app-blog/src/App.jsx
+++ apps/react-app-blog/src/App.jsx
@@ -1,12 +1,50 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Logo() {
+  return (
+    <a href="/">
+      <h1>Logo</h1>
+    </a>
+  );
+}
+
+function Menu() {
+  const menuList = [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "Sign In",
+      url: "/login",
+    }
+  ];
 
   return (
+    <ul>
+      {menuList.map((item) => {
+        return <li key={item.name}><a href={item.url}>{item.name}</a></li>
+      })}
+    </ul>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <div className='container'>
+        <Logo />
+        <Menu />
+      </div>
+    </header>
+  );
+}
+
+function App() {
+  return (
     <>
-      <p>Hello, World!</p>
+      <Header />
     </>
   )
 }
