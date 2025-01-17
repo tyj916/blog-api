7dd521908d2a1a85e6f5f7dfac096ae3ac6060a8
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-17 23:24:23 +0800

    Add blog posts template at home page

--- apps/react-app-blog/src/App.jsx
+++ apps/react-app-blog/src/App.jsx
@@ -2,11 +2,42 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css'
 
+function Post() {
+  return (
+    <div>
+      <p>Author</p>
+      <h3>Post Title</h3>
+      <p>Short Description</p>
+      <p>Posted Time</p>
+    </div>
+  );
+}
+
+function Posts() {
+  const posts = [];
+
+  return (
+    <div className="posts">
+      {!posts && <p>Loading...</p>}
+      {posts && posts.length > 0 && (
+        <div>
+          {posts.map((post) => {
+            return <Post key={post.id} />
+          })}
+        </div>
+      )}
+      {posts && posts.length === 0 && (
+        <p>No posts yet.</p>
+      )}
+    </div>
+  );
+}
+
 function Main() {
   return (
     <main>
       <div className='container'>
-        <p>Temporary</p>
+        <Posts />
       </div>
     </main>
   );
