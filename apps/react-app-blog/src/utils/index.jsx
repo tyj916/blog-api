601ea37cba279950df822d3e9f37c1350b23e778
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-18 23:23:54 +0800

    Add get time difference function

--- apps/react-app-blog/src/utils/index.jsx
+++ apps/react-app-blog/src/utils/index.jsx
@@ -13,4 +13,46 @@ function createModal(content) {
   window.modalRoot.render(<Modal content={content} closeModal={closeModal} />);
 }
 
-export default createModal;
+function getTimeDifference(timestamp) {
+  const timeDifference = new Date() - new Date(timestamp);
+
+  const PER_MINUTE = 60 * 1000;
+  const PER_HOUR = 60 * PER_MINUTE;
+  const PER_DAY = 24 * PER_HOUR;
+  const PER_YEAR = 365 * PER_DAY;
+  
+  if (timeDifference > PER_YEAR) {
+    // return time in date DD MM YYYY format
+    return new Date(timestamp).toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+  }
+  
+  if (timeDifference > PER_DAY) {
+    // return time in date DD MM format
+    return new Date(timestamp).toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "short",
+    });
+  }
+  
+  if (timeDifference > PER_HOUR) {
+    // return time in hours
+    return Math.round(timeDifference / (60 * 60 * 1000)) + ' h';
+  }
+  
+  if (timeDifference > PER_MINUTE) {
+    // return time in minutes
+    return Math.round(timeDifference / (60 * 1000)) + ' m';
+  }
+
+  // return time in seconds
+  return Math.round(timeDifference / (1000)) + ' s';
+}
+
+export {
+  createModal,
+  getTimeDifference,
+}
