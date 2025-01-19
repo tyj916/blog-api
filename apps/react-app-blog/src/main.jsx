d563c3c127b60dec95db5ff6e4aa6f17be055b48
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-19 22:57:31 +0800

    Add react router dom

--- apps/react-app-blog/src/main.jsx
+++ apps/react-app-blog/src/main.jsx
@@ -2,13 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
 
 const rootNode = document.querySelector("#root");
 const root = createRoot(rootNode);
 
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
     <div id='modal-container'></div>
   </StrictMode>,
 )
