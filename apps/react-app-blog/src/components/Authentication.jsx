dd077c156de02b198ad247c619d06bf46c9bdf9f
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-19 22:13:41 +0800

    Add login form handler

--- apps/react-app-blog/src/components/Authentication.jsx
+++ apps/react-app-blog/src/components/Authentication.jsx
@@ -1,19 +1,67 @@
 import PropTypes from "prop-types";
 import { createModal } from "../utils";
+import { useState } from "react";
 
 function AuthenticationForm({type, text}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch('http://localhost:3000/api/login', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      })
+    })
+    .then((response) => {
+      if (response.status >= 400) {
+        throw new Error("Something is wrong with the server... Please try again later.");
+      }
+
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+      if (response.token) {
+        localStorage.setItem('jwt', response.token);
+      }
+    })
+    .catch(err => console.error(err));
+  }
+
   return (
     <div className="authentication-form-container">
       <h1 className="form-title">{text}</h1>
-      <form action={type === 'login' ? '/login' : '/signUp'} method="POST">
+      <form onSubmit={handleSubmit}>
         <ul>
           <li>
             <label htmlFor="username">Username</label>
-            <input name="username" type="text" id="username" required />
+            <input 
+              name="username" 
+              type="text" 
+              id="username" 
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              required 
+            />
           </li>
           <li>
             <label htmlFor="password">Password</label>
-            <input name="password" type="password" id="password" required />
+            <input 
+              name="password" 
+              type="password" 
+              id="password" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required 
+            />
           </li>
           {
             type === 'signUp' && <>
