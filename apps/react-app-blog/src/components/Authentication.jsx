f793eb90a5057ce6bb435e7b4bf3dd8d11fffa41
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-20 11:52:51 +0800

    Add sign up form

--- apps/react-app-blog/src/components/Authentication.jsx
+++ apps/react-app-blog/src/components/Authentication.jsx
@@ -2,6 +2,108 @@ import PropTypes from "prop-types";
 import { createModal } from "../utils";
 import { useState } from "react";
 
+function SignUpForm({text}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError(null);
+    setLoading(true);
+
+    fetch('http://localhost:3000/api/register', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        displayName,
+      }),
+    })
+    .then((response) => {
+      if (response.status >= 500) {
+        throw new Error("Something is wrong with the server... Please try again later.");
+      }
+
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err);
+      setError(err);
+    })
+    .finally(() => setLoading(false));
+  }
+
+  return (
+    <div className="authentication-form-container">
+      <h1 className="form-title">{text}</h1>
+      <form onSubmit={handleSubmit} >
+        <ul>
+          <li>
+            <label htmlFor="username">Username</label>
+            <input 
+              name="username" 
+              type="text" 
+              id="username" 
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              required 
+            />
+          </li>
+          <li>
+            <label htmlFor="password">Password</label>
+            <input 
+              name="password" 
+              type="password" 
+              id="password" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required 
+            />
+          </li>
+          <li>
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input 
+              name="confirmPassword" 
+              type="password" 
+              id="confirm-password" 
+              value={confirmPassword}
+              onChange={e => setConfirmPassword(e.target.value)}
+              required 
+            />
+          </li>
+          <li>
+            <label htmlFor="display-name">Display Name (optional)</label>
+            <input 
+              name="displayName" 
+              type="text" 
+              id="display-name" 
+              value={displayName}
+              onChange={e => setDisplayName(e.target.value)}
+            />
+          </li>
+          {error && <li><p>{error}</p></li>}
+          {loading && <li><p>Loading...</p></li>}
+          <li>
+            <button type="submit">Sign Up</button>
+          </li>
+          <p>Already have an account? <button onClick={() => createModal(<AuthenticationForm type='login' text='Welcome Back.' />)}>Log In</button></p>
+        </ul>
+      </form>
+    </div>
+  );
+}
+
 function LoginForm({text}) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -90,6 +192,8 @@ function LoginForm({text}) {
 function AuthenticationForm({type, text}) {
   if (type === 'login') {
     return <LoginForm text={text} />
+  } else {
+    return <SignUpForm text={text} />
   }
 }
 
