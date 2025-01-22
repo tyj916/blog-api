3c56ddbd00a76eca0cbd3773197dd806d8ca88e8
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-22 17:03:15 +0800

    Add content to post page

--- apps/react-app-blog/src/components/Post.jsx
+++ apps/react-app-blog/src/components/Post.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getTimeDifference } from "../utils";
 
 function Post() {
   const { postId } = useParams();
+  const [title, setTitle] = useState(null);
+  const [time, setTime] = useState(null);
+  const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/posts/${postId}`, {mode: 'cors'})
+      .then(response => {
+        if (response.status >= 400) {
+          throw new Error("Something is wrong with the server... Please try again later.");
+        }
+
+        return response.json();
+      })
+      .then(response => {
+        const time = getTimeDifference(response.updatedAt);
+        setTitle(response.title);
+        setTime(time);
+        setContent(response.content);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, [postId]);
 
   return (
     <div>
-      This is post {postId}
+      <h1>{title}</h1>
+      <p>{time}</p>
+      <section>{content}</section>
     </div>
   );
 }
