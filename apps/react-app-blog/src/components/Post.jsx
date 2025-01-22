010a958ae5eb22d51a7eeab9ab7bec0f82f0802d
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-22 17:06:37 +0800

    Add author to post

--- apps/react-app-blog/src/components/Post.jsx
+++ apps/react-app-blog/src/components/Post.jsx
@@ -7,6 +7,7 @@ function Post() {
   const [title, setTitle] = useState(null);
   const [time, setTime] = useState(null);
   const [content, setContent] = useState(null);
+  const [author, setAuthor] = useState(null);
 
   useEffect(() => {
     fetch(`http://localhost:3000/api/posts/${postId}`, {mode: 'cors'})
@@ -19,9 +20,13 @@ function Post() {
       })
       .then(response => {
         const time = getTimeDifference(response.updatedAt);
+        const author = response.author;
+        const authorName = author.displayName || author.username;
+
         setTitle(response.title);
         setTime(time);
         setContent(response.content);
+        setAuthor(authorName);
       })
       .catch(err => {
         console.error(err);
@@ -31,7 +36,7 @@ function Post() {
   return (
     <div>
       <h1>{title}</h1>
-      <p>{time}</p>
+      <p>{author} {time}</p>
       <section>{content}</section>
     </div>
   );
