eb56737dfaadde4e1080cbb0e38d52460cc20aad
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-22 16:21:34 +0800

    Update component name

--- apps/react-app-blog/src/App.jsx
+++ apps/react-app-blog/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import Posts from './components/Posts';
+import PostList from './components/PostList';
 import Footer from './components/Footer';
 import './App.css'
 
@@ -8,7 +8,7 @@ function Main() {
   return (
     <main>
       <div className='container'>
-        <Posts />
+        <PostList />
       </div>
     </main>
   );
