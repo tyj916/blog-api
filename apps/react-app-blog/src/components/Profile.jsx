784da7421714c19f34a45f8720021aa6ac8b4988
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-24 16:17:23 +0800

    Add user written posts template

--- apps/react-app-blog/src/components/Profile.jsx
+++ apps/react-app-blog/src/components/Profile.jsx
@@ -9,7 +9,7 @@ function Profile() {
     const jwt = JSON.parse(localStorage.getItem('jwt'));
     const { userId } = jwt;
     
-    fetch(`http://localhost:3000/api/users/${userId}`, { mode: 'cors' })
+    fetch(`http://localhost:3000/api/users/${userId}/posts`, { mode: 'cors' })
       .then((response) => {
         if (response.status >= 400) {
           throw new Error("Something is wrong with the server... Please try again later.");
@@ -31,6 +31,8 @@ function Profile() {
   return (
     <section>
       <h1>{user.username}</h1>
+      <h2>{user.displayName}</h2>
+      {user.writtenPost.length > 0 ? <p>Have posts</p>  : <p>No post yet.</p>}
     </section>
   );
 }
