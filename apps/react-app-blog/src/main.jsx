25ea75c111b156c9720fabca92976f03206b5142
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-27 13:54:15 +0800

    Refactor router

--- apps/react-app-blog/src/main.jsx
+++ apps/react-app-blog/src/main.jsx
@@ -1,45 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 import './index.css'
-import App from './App.jsx'
-import Post from './components/Post.jsx'
-import Home from './components/Home.jsx';
-import Profile from './components/Profile.jsx'
-import NotFound from './components/error/NotFound.jsx';
-import Unauthorized from './components/error/Unauthorized.jsx';
-import AuthenticationForm from './components/Authentication.jsx';
-import { isLoggedIn } from './utils/index.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: '/posts/:postId', element: <Post />, errorElement: <NotFound /> },
-      { 
-        path: '/profile', 
-        element: isLoggedIn() ? <Profile /> : <Navigate to='/login' />, 
-        errorElement: <Unauthorized /> 
-      },
-      { path: '/profile/:username', element: <Profile />, errorElement: <NotFound /> },
-    ],
-    errorElement: <NotFound />,
-  },
-  {
-    path: '/login', 
-    element: <AuthenticationForm type='login' text='Welcome Back.' />
-  },
-  {
-    path: '/register',
-    element: <AuthenticationForm type='signUp' text='Join Us.' />
-  },
-  { 
-    path: '/register/writer', 
-    element: <AuthenticationForm type='signUp' text='Create an account to start writing.' /> 
-  },
-]);
 
 const rootNode = document.querySelector("#root");
 const root = createRoot(rootNode);
---
+++ apps/react-app-blog/src/routes.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import App from './App.jsx'
+import Post from './components/Post.jsx'
+import Home from './components/Home.jsx';
+import Profile from './components/Profile.jsx'
+import NotFound from './components/error/NotFound.jsx';
+import Unauthorized from './components/error/Unauthorized.jsx';
+import AuthenticationForm from './components/Authentication.jsx';
+import { isLoggedIn } from './utils/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/posts/:postId', element: <Post />, errorElement: <NotFound /> },
+      { 
+        path: '/profile', 
+        element: isLoggedIn() ? <Profile /> : <Navigate to='/login' />, 
+        errorElement: <Unauthorized /> 
+      },
+      { path: '/profile/:username', element: <Profile />, errorElement: <NotFound /> },
+    ],
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/login', 
+    element: <AuthenticationForm type='login' text='Welcome Back.' />
+  },
+  {
+    path: '/register',
+    element: <AuthenticationForm type='signUp' text='Join Us.' />
+  },
+  { 
+    path: '/register/writer', 
+    element: <AuthenticationForm type='signUp' text='Create an account to start writing.' /> 
+  },
+]);
+
+export default router;
