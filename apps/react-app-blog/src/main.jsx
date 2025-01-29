ce478c915a15637d3400cea29cdbcea9c678c3b5
Tan Yi Jun <tanyijun6@gmail.com>
2025-01-29 15:07:54 +0800

    Add normalize css

--- apps/react-app-blog/src/main.jsx
+++ apps/react-app-blog/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
+import 'normalize.css';
 import './index.css'
 
 const rootNode = document.querySelector("#root");
