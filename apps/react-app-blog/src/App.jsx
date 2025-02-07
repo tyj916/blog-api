db08afab52053a52f4ef2c2d23daee9234ee58f4
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-07 23:56:48 +0800

    Adjust module classes

--- apps/react-app-blog/src/App.jsx
+++ apps/react-app-blog/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
-import './styles/App.css';
+import './styles/App.module.css';
 
 function App() {
   return (
