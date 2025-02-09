6271c712ce402937f13bc274da887ad2687a4303
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-09 23:55:42 +0800

    Fix all paragraph margin went 8px

--- apps/react-app-blog/src/components/Footer.jsx
+++ apps/react-app-blog/src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import '../styles/Footer.module.css';
+import styles from '../styles/Footer.module.css';
 
 function Footer() {
   return (
     <footer>
-      <p className="copyright">
+      <p className={styles.copyright}>
         &copy; 2025 <a href="https://github.com/tyj916">tyj916</a>
       </p>
     </footer>
