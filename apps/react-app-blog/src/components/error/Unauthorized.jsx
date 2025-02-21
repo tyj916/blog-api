bf0ef22ac7c9523888fc224819f34f4498b76264
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-21 14:58:01 +0800

    Add style to unauthorized page

--- apps/react-app-blog/src/components/error/Unauthorized.jsx
+++ apps/react-app-blog/src/components/error/Unauthorized.jsx
@@ -1,15 +1,18 @@
 import { useNavigate } from "react-router-dom";
+import styles from '../../styles/index.css';
 
 function Unauthorized() {
   const navigate = useNavigate();
 
   return (
-    <section>
-      <h1>401</h1>
-      <h2>Hold up. Authentication required.</h2>
-      <p>Try login if you think you have access to it.</p>
-      <p>Or turn back to where you came from.</p>
-      <button onClick={() => navigate(-1)}>Send Me Back</button>
+    <section className={styles.error}>
+      <div className={styles.container}>
+        <h1>401</h1>
+        <h2>Hold up. Authentication required.</h2>
+        <p>Try login if you think you have access to it.</p>
+        <p>Or turn back to where you came from.</p>
+        <button onClick={() => navigate(-1)}>Send Me Back</button>
+      </div>
     </section>
   );
 }
