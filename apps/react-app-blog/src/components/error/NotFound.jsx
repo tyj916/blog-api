12d3fb799d944699da47608c14c644cbfffaed9c
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-21 14:56:20 +0800

    Add style to error 404 not found page

--- apps/react-app-blog/src/components/error/NotFound.jsx
+++ apps/react-app-blog/src/components/error/NotFound.jsx
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import styles from '../../styles/Error.module.css';
 
 function NotFound() {
   const navigate = useNavigate();
 
   return (
-    <section>
-      <h1>404</h1>
-      <h2>Sorry we couldn&apos;t find that page</h2>
-      <p>We don&apos;t know how you ended up here, but let the portal magic send you back to where you came from</p>
-      <button onClick={() => navigate(-1)}>Send Me Back</button>
+    <section className={styles.error}>
+      <div className={styles.container}>
+        <h1>404</h1>
+        <h2>Sorry we couldn&apos;t find that page</h2>
+        <p>We don&apos;t know how you ended up here, but let the portal magic send you back to where you came from</p>
+        <button onClick={() => navigate(-1)}>Send Me Back</button>
+      </div>
     </section>
   );
 }
