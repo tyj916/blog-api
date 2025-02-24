c8507a6918cd33449422ab1afb47f8eada9a39e9
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-24 17:18:40 +0800

    Fix html table missing thead and tbody

--- apps/react-app-editor/src/components/Dashboard.jsx
+++ apps/react-app-editor/src/components/Dashboard.jsx
@@ -7,25 +7,29 @@ function Dashboard() {
         <h1>Dashboard</h1>
         <h2>Your Posts</h2>
         <table>
-          <tr>
-            <th>Title</th>
-            <th>Status</th>
-            <th>Last Update</th>
-            <th></th>
-          </tr>
-          {posts && posts.map(post => {
-            return (
-              <tr key={post.id}>
-                <td><a href={`/post/${post.id}`}>{post.title}</a></td>
-                <td>{post.status}</td>
-                <td>{post.updatedAt}</td>
-                <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                </td>
-              </tr>
-            )
-          })}
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Status</th>
+              <th>Last Update</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts && posts.map(post => {
+              return (
+                <tr key={post.id}>
+                  <td><a href={`/post/${post.id}`}>{post.title}</a></td>
+                  <td>{post.status}</td>
+                  <td>{post.updatedAt}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
       </div>
     </>
