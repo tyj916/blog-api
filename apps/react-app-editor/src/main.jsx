a966a67e38bcb57154d55647a5e94fcb86333ec4
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-24 14:59:02 +0800

    Add react router

--- apps/react-app-editor/src/main.jsx
+++ apps/react-app-editor/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
