f537d973cece53b9caecb5dff01f263d0b07a03d
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-24 15:10:14 +0800

    Create website structure

--- apps/react-app-editor/src/App.jsx
+++ apps/react-app-editor/src/App.jsx
@@ -1,10 +1,19 @@
 import './styles/App.css'
 
 function App() {
-
   return (
     <>
-    
+      <nav>
+        <div>
+          <a href="/"><h1>title</h1></a>
+          <ul>
+            <li><a href="/">Dashboard</a></li>
+            <li><a href="/new">Create New</a></li>
+            <li><a href="/logout">Log Out</a></li>
+          </ul>
+        </div>
+      </nav>
+      <main>Main</main>
     </>
   )
 }
