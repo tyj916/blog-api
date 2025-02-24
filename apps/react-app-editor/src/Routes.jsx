4a104bbbc843deeae4e2cfacc7be72520d7b47cc
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-24 15:45:49 +0800

    Create post editor

--- apps/react-app-editor/src/Routes.jsx
+++ apps/react-app-editor/src/Routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import Dashboard from './components/Dashboard.jsx';
+import Editor from './components/Editor.jsx';
 
 function MyRoutes() {
   return (
     <Routes>
       <Route path='/' element={<App />}>
         <Route index element={<Dashboard />} />
+        <Route path='/new' element={<Editor />} />
       </Route>
     </Routes>
   )
---
+++ apps/react-app-editor/src/components/Editor.jsx
@@ -0,0 +1,18 @@
+function Editor() {
+  return (
+    <div>
+      <h1>Editor</h1>
+      <section>
+        <div>
+          Editor here
+        </div>
+        <div>
+          <button>Save to draft</button>
+          <button>Publish</button>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Editor;
