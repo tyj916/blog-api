8981f4ecafc8c513103d37b4ba68c8215265fd27
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-24 15:52:15 +0800

    Add 404 not found page

--- apps/react-app-editor/src/Routes.jsx
+++ apps/react-app-editor/src/Routes.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import Editor from './components/Editor.jsx';
+import NotFound from './components/error/NotFound.jsx';
 
 function MyRoutes() {
   return (
     <Routes>
       <Route path='/' element={<App />}>
         <Route index element={<Dashboard />} />
-        <Route path='/new' element={<Editor />} />
+        <Route path='new' element={<Editor />} />
+        <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
   )
