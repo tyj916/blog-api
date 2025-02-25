fd41030ae0294266bc11982a50f9a40826ee598f
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-25 18:17:02 +0800

    Add jwt to editor app to achieve SSO solution

--- apps/react-app-blog/src/main.jsx
+++ apps/react-app-blog/src/main.jsx
@@ -9,8 +9,18 @@ const rootNode = document.querySelector("#root");
 const root = createRoot(rootNode);
 const router = createBrowserRouter(routes);
 
+// send jwt to editor app
+function shareJwtWithEditorApp() {
+  const jwt = localStorage.getItem('jwt');
+  const iframe = document.querySelector('#shareJwt');
+  iframe.contentWindow.postMessage(JSON.stringify(jwt), import.meta.env.VITE_EDITOR_URL);
+}
+
 root.render(
   <StrictMode>
     <RouterProvider router={router} />
+
+    {/* this iframe is used to share jwt with editor app */}
+    <iframe id='shareJwt' src={import.meta.env.VITE_EDITOR_URL} style={{display: 'none'}} onLoad={shareJwtWithEditorApp}></iframe>
   </StrictMode>,
 )
--- apps/react-app-editor/src/main.jsx
+++ apps/react-app-editor/src/main.jsx
@@ -4,6 +4,18 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import MyRoutes from './Routes'
 
+// the script is used to receive jwt from blog app
+window.addEventListener("message", (e) => {
+  if (e.origin !== import.meta.env.VITE_BLOG_APP_URL) {
+    return;
+  }
+
+  const jwt = JSON.parse(e.data);
+  if (jwt) {
+    localStorage.setItem('jwt', jwt);
+  }
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
