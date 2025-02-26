1a8df550166f419db0b2afee72917a94610f3f58
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-26 16:20:37 +0800

    Add jwt removed check on load

--- apps/react-app-blog/src/main.jsx
+++ apps/react-app-blog/src/main.jsx
@@ -4,17 +4,26 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
 import 'normalize.css';
 import './styles/index.css'
-import { postLogInMessage } from './utils';
+import { isLoggedIn } from './utils';
 
 const rootNode = document.querySelector("#root");
 const root = createRoot(rootNode);
 const router = createBrowserRouter(routes);
+
+// sync localStorage jwt with editor app
+function postJwtMessage() {
+  const iframe = document.querySelector('#shareLocalStorage');
+  iframe.contentWindow.postMessage(JSON.stringify({
+    message: isLoggedIn() ? 'login' : 'logout',
+    jwt: localStorage.getItem('jwt'),
+  }), import.meta.env.VITE_EDITOR_URL);
+}
 
 root.render(
   <StrictMode>
     <RouterProvider router={router} />
 
     {/* this iframe is used to share localstorage with editor app */}
-    <iframe id='shareLocalStorage' src={import.meta.env.VITE_EDITOR_URL} style={{display: 'none'}} onLoad={postLogInMessage}></iframe>
+    <iframe id='shareLocalStorage' src={import.meta.env.VITE_EDITOR_URL} style={{display: 'none'}} onLoad={postJwtMessage}></iframe>
   </StrictMode>,
 )
