67f5711d0bcbbac329bbe312eb291e20f18698f2
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-26 15:07:41 +0800

    Add redirect login at editor app

--- apps/react-app-editor/src/Routes.jsx
+++ apps/react-app-editor/src/Routes.jsx
@@ -3,14 +3,17 @@ import App from './App.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import Editor from './components/Editor.jsx';
 import NotFound from './components/error/NotFound.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 function MyRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<App />}>
-        <Route index element={<Dashboard />} />
-        <Route path='new' element={<Editor />} />
-        <Route path='*' element={<NotFound />} />
+      <Route element={<ProtectedRoute/>}>
+        <Route path='/' element={<App />}>
+          <Route index element={<Dashboard />} />
+          <Route path='new' element={<Editor />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Route>
     </Routes>
   )
---
+++ apps/react-app-editor/src/components/ProtectedRoute.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from "react-router-dom";
+
+function isJwtExpired(jwt) {
+  const expiryTime = (7 * 24 * 60 * 60 * 1000) - (new Date() - new Date(jwt.timestamp));
+  return expiryTime > 0;
+}
+
+function isLoggedIn() {
+  if (!localStorage.getItem('jwt')) {
+    return false;
+  }
+
+  const jwt = JSON.parse(localStorage.getItem('jwt'));
+  return isJwtExpired(jwt);
+}
+
+function ProtectedRoute() {
+  if (isLoggedIn()) {
+    return <Outlet />
+  } else {
+    window.location.href = import.meta.env.VITE_BLOG_APP_URL + '/login/?from=' + window.location.href;
+  }
+}
+
+export default ProtectedRoute;
