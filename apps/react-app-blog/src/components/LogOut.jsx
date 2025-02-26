3cb5bdef80e0f9eef16768ea69e7150d167c16d0
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-26 16:35:01 +0800

    Fix logout does not post message to editor app

--- apps/react-app-blog/src/components/LogOut.jsx
+++ apps/react-app-blog/src/components/LogOut.jsx
@@ -1,15 +1,6 @@
-import { Navigate } from "react-router-dom";
-
-function postLogOutMessage() {
-  const iframe = document.querySelector('#shareLocalStorage');
-  iframe.contentWindow.postMessage(JSON.stringify({message: 'logout'}), import.meta.env.VITE_EDITOR_URL);
-}
-
 function LogOut() {
   localStorage.removeItem('jwt');
-  postLogOutMessage();
-
-  return <Navigate to='/' />
+  window.location.href = '/';
 }
 
 export default LogOut;
