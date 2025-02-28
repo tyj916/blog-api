fb63336bffb34e868fd9a11b27ad7ecdffe15fcf
Tan Yi Jun <tanyijun6@gmail.com>
2025-02-28 16:24:20 +0800

    Replace table with css grid structure for flexibility

--- apps/react-app-editor/src/components/Dashboard.jsx
+++ apps/react-app-editor/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { convertTimestamp, getCurrentUsername } from "../utils";
+import styles from '../styles/Dashboard.module.css';
 
 function Dashboard() {
   const [posts, setPosts] = useState();
@@ -21,32 +22,30 @@ function Dashboard() {
       <div>
         <h1>Dashboard</h1>
         <h2>Your Posts</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Status</th>
-              <th>Last Update</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {loading && <p>Loading...</p>}
+        {loading && <p>Loading...</p>}
+        <section className={styles.postsTable}>
+          <div className={[styles.tableRow, styles.tableHead].join(' ')}>
+            <p>Title</p>
+            <p>Status</p>
+            <p>Last Update</p>
+            <p></p>
+          </div>
+          <div className={styles.tableBody}>
             {posts && posts.map(post => {
               return (
-                <tr key={post.id}>
-                  <td><a href={`/post/${post.id}`}>{post.title}</a></td>
-                  <td>{post.status}</td>
-                  <td>{convertTimestamp(post.updatedAt)}</td>
-                  <td>
+                <div key={post.id} className={[styles.tableRow, styles.rowItem].join(' ')}>
+                  <p><a href={`/post/${post.id}`}>{post.title}</a></p>
+                  <p>{post.status}</p>
+                  <p>{convertTimestamp(post.updatedAt)}</p>
+                  <p>
                     <button>Edit</button>
                     <button>Delete</button>
-                  </td>
-                </tr>
+                  </p>
+                </div>
               )
             })}
-          </tbody>
-        </table>
+          </div>
+        </section>
       </div>
     </>
   );
