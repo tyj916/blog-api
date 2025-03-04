c764a6c298af5bc30799bf611aad9ea3e9aaf1ff
Tan Yi Jun <tanyijun6@gmail.com>
2025-03-04 19:32:34 +0800

    Add get user data by username api

--- services/api/src/controllers/user.js
+++ services/api/src/controllers/user.js
@@ -35,6 +35,16 @@ async function getUserByUserId(req, res) {
   }
 }
 
+async function getUserByUsername(req, res) {
+  try {
+    const { username } = req.params;
+    const user = await db.user.getUserByUsername(username);
+    return res.send(user);
+  } catch(err) {
+    console.error(err);
+  }
+}
+
 async function getUserPostsByUserId(req, res) {
   try {
     const userId = req.params.userId;
@@ -84,6 +94,7 @@ module.exports = {
   createUser,
   getAllUsers,
   getUserByUserId,
+  getUserByUsername,
   getUserPostsByUserId,
   getUserPostsByUsername,
   updateUser,
