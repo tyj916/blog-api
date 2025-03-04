a52d4476484c1d95e7ef55b8585a935610bc8398
Tan Yi Jun <tanyijun6@gmail.com>
2025-03-04 19:22:22 +0800

    Fix author id and username conflict

--- services/api/src/routes/post.js
+++ services/api/src/routes/post.js
@@ -6,8 +6,8 @@ const router = Router();
 
 router.get('/', controllers.post.getAllPosts);
 router.get('/published', controllers.post.getPublishedPosts);
-router.get('/author/:username', controllers.post.getPostsByAuthorUsername);
-router.get('/author/:authorId', controllers.post.getPostsByAuthorId);
+router.get('/author/id/:authorId', controllers.post.getPostsByAuthorId);
+router.get('/author/username/:username', controllers.post.getPostsByAuthorUsername);
 router.get('/:postId', controllers.post.getPostByPostId);
 
 router.use(passport.authenticate('jwt', { session: false }));
