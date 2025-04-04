1d31fa7217619a3d91cf8dfe6baad84da0fd4fd7
Tan Yi Jun <tanyijun6@gmail.com>
2025-04-04 22:00:20 +0800

    Test production environment

--- apps/react-app-editor/src/main.jsx
+++ apps/react-app-editor/src/main.jsx
@@ -7,21 +7,21 @@ import MyRoutes from './Routes'
 
 // the script is used to sync localStorage with blog app
 window.addEventListener("message", (e) => {
-  const originURL = e.origin.charAt(e.origin.length - 1) === '/' ? e.origin : e.origin + '/';
-  alert(originURL);
-  if (originURL !== import.meta.env.VITE_BLOG_APP_URL + '/') {
-    alert('rejected');
-    return;
-  }
+  if (
+    e.origin === import.meta.env.VITE_BLOG_APP_URL
+    || e.origin === import.meta.env.VITE_BLOG_APP_URL + '/'
+  ) {
+    const data = JSON.parse(e.data);
+    if (data.message === 'login') {
+      localStorage.setItem('jwt', data.jwt);
+      return;
+    }
 
-  const data = JSON.parse(e.data);
-  if (data.message === 'login') {
-    localStorage.setItem('jwt', data.jwt);
-    return;
-  }
-
-  if (data.message === 'logout') {
-    localStorage.removeItem('jwt');
+    if (data.message === 'logout') {
+      localStorage.removeItem('jwt');
+      return;
+    }
+  } else {
     return;
   }
 });
