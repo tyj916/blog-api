34c3c5a5ad61ea3f84ae12606ad78a06b861a026
Tan Yi Jun <tanyijun6@gmail.com>
2025-04-05 13:42:52 +0800

    Fix authentication css file missing

--- apps/react-app-blog/src/main.jsx
+++ apps/react-app-blog/src/main.jsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
 import 'normalize.css';
 import './styles/index.css'
-import { postJwtMessage } from './utils';
 
 const rootNode = document.querySelector("#root");
 const root = createRoot(rootNode);
@@ -13,18 +12,5 @@ const router = createBrowserRouter(routes);
 root.render(
   <StrictMode>
     <RouterProvider router={router} />
-
-    {/* this iframe is used to share localstorage with editor app */}
-    <iframe id='shareLocalStorage' src={import.meta.env.VITE_EDITOR_URL + '/'} style={{display: 'none'}}></iframe>
   </StrictMode>,
 )
-
-window.addEventListener("message", (e) => {
-  if (e.origin !== import.meta.env.VITE_EDITOR_URL) {
-    console.log(`Invalid message origin: ${e.origin}, ${import.meta.VITE_BLOG_APP_URL}`);
-    return;
-  }
-  console.log(e.data);
-  console.log('Start to post message...');
-  postJwtMessage();
-})
---
+++ apps/react-app-editor/src/components/Authentication.jsx
@@ -0,0 +1,244 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import styles from '../styles/Authentication.module.css';
+
+const { VITE_API_URL } = import.meta.env;
+
+function handleLogin(username, password, setMessage, setLoading) {
+  fetch(`${VITE_API_URL}/login`, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+    redirect: 'manual'
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.token) {
+        const { userId, username, token } = data;
+        localStorage.setItem('jwt', JSON.stringify({
+          userId,
+          username,
+          token,
+          timestamp: new Date(),
+        }));
+        window.location.href = '/';
+      }
+  
+      setMessage(data.message);
+    })
+    .catch(err => console.error(err))
+    .finally(() => setLoading(false));
+}
+
+function LoginForm({text, backUrl}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage('');
+    setLoading(true);
+
+    handleLogin(username, password, setMessage, setLoading);
+  }
+
+  return (
+    <div className={styles.formContainer}>
+      <h1 className={styles.title}>{text}</h1>
+      <form onSubmit={handleSubmit} className={styles.form} >
+        <ul className={styles.formItems}>
+          <li>
+            <label htmlFor="username">Username</label>
+            <input 
+              name="username" 
+              type="text" 
+              id="username" 
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              required 
+            />
+          </li>
+          <li>
+            <label htmlFor="password">Password</label>
+            <input 
+              name="password" 
+              type="password" 
+              id="password" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required 
+            />
+          </li>
+          {loading && <li><p>Loading...</p></li>}
+          {message && <li><p>{message}</p></li>}
+          <li>
+            <button type="submit">Log In</button>
+          </li>
+          <p>No Account? <Link 
+            to={{
+              pathname: '/register',
+              search: `?from=${backUrl}`,
+            }} className={styles.otherOptions}>Create One</Link></p>
+        </ul>
+      </form>
+    </div>
+  );
+}
+
+function SignUpForm({text, backUrl}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage('');
+    setLoading(true);
+
+    fetch(`${VITE_API_URL}/register`, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        displayName,
+      }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.username) {
+        let count = 5;
+        const countdown = setInterval(() => {
+          if (count >= 0) {
+            setMessage(`You are registered! You'll be login automatically in ${count} sec...`);
+            count--;
+          } else {
+            clearInterval(countdown);
+            handleLogin(username, password, setMessage, setLoading);
+          }
+        }, 1000);
+      } else {
+        setMessage(data.message);
+      }
+    })
+    .catch(err => console.error(err))
+    .finally(() => setLoading(false));
+  }
+
+  return (
+    <div className={styles.formContainer}>
+      <h1 className={styles.title}>{text}</h1>
+      <form onSubmit={handleSubmit} className={styles.form} >
+        <ul className={styles.formItems}>
+          <li>
+            <label htmlFor="username">Username</label>
+            <input 
+              name="username" 
+              type="text" 
+              id="username" 
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              required 
+            />
+          </li>
+          <li>
+            <label htmlFor="password">Password</label>
+            <input 
+              name="password" 
+              type="password" 
+              id="password" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required 
+            />
+          </li>
+          <li>
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input 
+              name="confirmPassword" 
+              type="password" 
+              id="confirm-password" 
+              value={confirmPassword}
+              onChange={e => setConfirmPassword(e.target.value)}
+              required 
+            />
+            {confirmPassword && password !== confirmPassword && <p>Password doesn&apos;t match.</p>}
+          </li>
+          <li>
+            <label htmlFor="display-name">Display Name (optional)</label>
+            <input 
+              name="displayName" 
+              type="text" 
+              id="display-name" 
+              value={displayName}
+              onChange={e => setDisplayName(e.target.value)}
+            />
+          </li>
+          {loading && <li><p>Loading...</p></li>}
+          {message && <li><p>{message}</p></li>}
+          <li>
+            <button type="submit">Sign Up</button>
+          </li>
+          <p>Already have an account? <Link 
+            to={{
+              pathname: '/login',
+              search: `?from=${backUrl}`,
+            }} className={styles.otherOptions}>Log In</Link></p>
+        </ul>
+      </form>
+    </div>
+  );
+}
+
+function AuthenticationForm({type = 'login', text = 'Welcome Back.'}) {
+  const [searchParams] = useSearchParams();
+  const backUrl = searchParams.get('from') || import.meta.env.VITE_BLOG_APP_URL;
+
+  return (
+    <div id={styles.authForm}>
+      <div className={styles.container}>
+        <div>
+          <Link to={backUrl}>&lt; Back</Link>
+          {
+            type === 'login' 
+              ? <LoginForm text={text} backUrl={backUrl} /> 
+              : <SignUpForm text={text} backUrl={backUrl} />
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+LoginForm.propTypes = {
+  text: PropTypes.string,
+  backUrl: PropTypes.string,
+}
+
+SignUpForm.propTypes = {
+  text: PropTypes.string,
+  backUrl: PropTypes.string,
+}
+
+AuthenticationForm.propTypes = {
+  type: PropTypes.string,
+  text: PropTypes.string,
+  targetUrl: PropTypes.string,
+}
+
+export default AuthenticationForm;
