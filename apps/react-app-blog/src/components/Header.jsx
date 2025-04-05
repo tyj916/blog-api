0469c42f02ebc9f335786b9cf2078e8555160e5d
Tan Yi Jun <tanyijun6@gmail.com>
2025-04-06 01:27:11 +0800

    Add blog posts link to logged in user

--- apps/react-app-blog/src/components/Header.jsx
+++ apps/react-app-blog/src/components/Header.jsx
@@ -36,6 +36,7 @@ function Links() {
     return (
       <>
         <li><Link to="/">Home</Link></li>
+        <li><Link to="/posts">Blog</Link></li>
         <li><Link to={import.meta.env.VITE_EDITOR_URL}>Write</Link></li>
         <li><Link to='/profile'>Profile</Link></li>
         <li><Link to='/logout'>Log Out</Link></li>
