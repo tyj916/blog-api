d0decc533dd2b56384c9b890ad9c7341dcbb603f
Tan Yi Jun <tanyijun6@gmail.com>
2025-04-07 18:10:26 +0800

    Add shared localstorage

--- apps/react-app-blog/src/main.jsx
+++ apps/react-app-blog/src/main.jsx
@@ -5,6 +5,21 @@ import routes from './routes';
 import 'normalize.css';
 import './styles/index.css'
 
+window.addEventListener('message', (e) => {
+  if (e.origin !== import.meta.env.VITE_EDITOR_URL) {
+    console.log('Origin is incorrect. Expected: ' + import.meta.env.VITE_EDITOR_URL + '; Received: ' + e.origin);
+    return;
+  } else {
+    console.log('blog app good to go');
+  }
+
+  const data = JSON.parse(e.data);
+  if (data.type === 'getJwt') {
+    const jwt = localStorage.getItem('jwt');
+    e.source.postMessage(JSON.stringify(jwt), '*');
+  }
+});
+
 const rootNode = document.querySelector("#root");
 const root = createRoot(rootNode);
 const router = createBrowserRouter(routes);
--- apps/react-app-editor/src/main.jsx
+++ apps/react-app-editor/src/main.jsx
@@ -5,8 +5,25 @@ import 'normalize.css';
 import './styles/index.css'
 import MyRoutes from './Routes'
 
+window.addEventListener('message', (e) => {
+  if (e.origin !== import.meta.env.VITE_BLOG_APP_URL) {
+    console.log('Origin is incorrect. Expected: ' + import.meta.env.VITE_BLOG_APP_URL + '; Received: ' + e.origin);
+    return;
+  } else {
+    console.log('editor good to go');
+  }
+
+  const data = JSON.parse(e.data);
+  // localStorage.setItem('jwt', data);
+  console.log(data);
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <iframe id='sharedStorage' src={import.meta.env.VITE_BLOG_APP_URL} style={{'display': 'none'}} onLoad={() => {
+      const iframe = document.querySelector('#sharedStorage');
+      iframe.contentWindow.postMessage(JSON.stringify({ type: 'getJwt' }), import.meta.env.VITE_BLOG_APP_URL);
+    }}></iframe>
     <BrowserRouter>
       <MyRoutes />
     </BrowserRouter>
